33e1a4ad2f1829a6ea09d4f3186bc3fa1621e67c
ruantianyi <ruantianyi10@gmail.com>
2026-06-26 14:35:10 -0400

    chore: remove unused automation scripts and knowledge base modules

--- extract_js.ps1
+++
@@ -1,4 +0,0 @@
-$html = Get-Content 7.0.html -Raw
-$scripts = [regex]::Matches($html, '(?si)<script[^>]*>(.*?)</script>')
-$scripts | ForEach-Object { $_.Groups[1].Value } | Out-File -FilePath test.js
-cscript //E:JScript test.js
